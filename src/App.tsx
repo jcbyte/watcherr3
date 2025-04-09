0b3d82ca1648bb9202485b2d21434591723a8f2e
Joel Cutler <joelcutler108@gmail.com>
2025-04-09 13:17:24 +0100

    added shadecn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
 	return (
 		<>
 			<div>Hello World</div>
+			<Button variant="default">Button</Button>
 		</>
 	);
 }
